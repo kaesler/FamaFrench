d8a04e956ea1e25f650f90072590a8b0773a2040
Kevin Esler <kevin.esler@alum.bu.edu>
2011-01-08 17:56:48 -0500

    Add raw data sources

---
+++ 2010/hacking/FamaFrench/src/kae/ff3/FamaFrenchDatumInternational.scala
@@ -0,0 +1,56 @@
+package kae.ff3
+import scala.io.Source
+
+class FamaFrenchDatumIntarnational(
+  val month: Month,
+  val metrics: FamaFrenchMetrics
+) extends Ordered[FamaFrenchDatumUsa]
+{
+//  // TODO: hashCode and equals
+//
+//  // Order by date
+//  def compare(that: FamaFrenchDatumUsa) : Int = {
+//    month.compare(that.month)
+//  }
+}
+
+object FamaFrenchDatumInternational
+{
+//  // This file was created by CMPT_ME_BEME_RETS using the 201011 CRSP database.
+//  // The 1-month TBill return is from Ibbotson and Associates, Inc.
+//  //
+//  //        Mkt-RF     SMB     HML      RF
+//  //192607    2.62   -2.16   -2.92    0.22
+//  private val lineRegexp = """([0-9][0-9][0-9][0-9][0-9][0-9]) +([^ ]+) +([^ ]+) +([^ ]+) +([^ ]+)""".r
+//
+//  def matches(line: String) : Boolean = {
+//    lineRegexp findFirstIn line match {
+//      case Some(s) => true
+//      case None => false  
+//    }
+//  }
+//  
+//  def create(line: String) : FamaFrenchDatumUsa = {
+//    val lineRegexp(s1, s2, s3, s4, s5) = line
+//    new FamaFrenchDatumUsa(new Month(s1),
+//    		               new FamaFrenchMetrics(s2.toDouble,
+//    		            		                 s3.toDouble,
+//    		            		                 s4.toDouble,	
+//    		            		                 s5.toDouble))
+//  }
+//  
+//  def parseFile : Seq[FamaFrenchDatumUsa] = {
+//    val file = FileLocater.locateUsaResearchMonthlyDataFile
+//    require(file.exists)
+//    
+//   	(Source.fromFile(file).getLines()
+//     // Drop the leading few lines that are not monthly data lines
+//     dropWhile { line => !matches(line) }
+//
+//     // Retain the monthly data lines
+//     takeWhile { line => matches(line) }
+//
+//     // Foreach monthly data line create a datum
+//     map { monthlyDataLine => create(monthlyDataLine) }).toSeq
+//  }
+}
---
+++ 2010/hacking/FamaFrench/src/kae/ff3/FamaFrenchDigestInternational.scala
@@ -0,0 +1,39 @@
+package kae.ff3
+import scala.collection.immutable.TreeMap
+
+object FamaFrenchDigestInternational extends FamaFrenchDigest
+{
+  // TODO: we must combine 3 data sources
+  //  1. Ken French's international data
+  //  3. Ken French's USA data (for risk free data)
+  //  4. 
+  private val rawInternationalData = FamaFrenchDatumInternational.parseFile
+  private val rawUsaData = FamaFrenchDatumUsa.parseFile
+
+  // Construct the above from the raw data below.
+  private def makeMetricsMap : TreeMap[Month, FamaFrenchMetrics] = {
+    var result = TreeMap[Month, FamaFrenchMetrics]()
+    
+    rawData foreach { datum =>
+      result += (datum.month -> datum.metrics)
+    }
+    result
+  }
+  private val metricsMap = makeMetricsMap 
+
+  private def validate = {
+	// Make sure there are no gaps. I.e. the successor of all the keys except
+	// the last one must be contained in the key set.
+	val lastMonth = metricsMap.keys.last
+	metricsMap.keys foreach { month =>
+	  if (month != lastMonth) {
+	    require(metricsMap.contains(month.successor))
+	  }
+	}
+  }
+
+  def getMetrics (month: Month) : FamaFrenchMetrics = metricsMap(month)
+
+  def months  = metricsMap.keySet
+
+}
