f883037e9dd23fe589010230987340cfc552f55a
Kevin Esler <kevin.esler@gmail.com>
2011-01-08 11:01:49 -0500

    progress

--- 2010/hacking/FamaFrench/src/kae/ff3/CmdFetchFromWeb.scala
+++ 2010/hacking/FamaFrench/src/kae/ff3/CmdFetchFromWeb.scala
@@ -3,7 +3,9 @@ package kae.ff3
 import java.net.URL
 import scala.io.Source
 
-object CmdFetchFromYahoo {
+object CmdFetchFromWeb {
+  val ffDailyZip = "http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/F-F_Research_Data_Factors_daily.zip"
+	  
   def urlForTicker(ticker: String) : String = {
     "http://ichart.finance.yahoo.com/table.csv?s=%s&a=00&b=1&c=1900&d=00&e=8&f=2011&g=d&ignore=.csv".format(ticker)
   }
